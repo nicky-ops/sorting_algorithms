1989fd6d58a20b14fb5eb08f03cea74d93153de1
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-26 02:36:32 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	size_t j;
+	int temp, swp;
+	size_t j, i, k;
 
 	if (array == NULL || size < 2)
 		return;
 	if (array)
 	{
-		for (j = 0; j < size -1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j]> array[j + 1])
+			swp = 0;
+			for (j = 0; j < size -1; j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				if (array[j]> array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					for (k = 0; k < size; k++)
+					{
+						printf("%d", array[k]);
+					}
+					swp = 1;
+				}
 			}
-		printf("%d", *array);
 		}
 	}
-}	
+}
