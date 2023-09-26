01720b91b6633b23d1e7543fd21d69f98b05bfff
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-26 07:49:07 -0700

    fixed betty errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,36 +10,33 @@
  */
 size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
 {
-    int pivot, temp;
-    ssize_t i, j;
+	int pivot, temp;
+	ssize_t i, j;
 
-    pivot = array[high];
-    i = low - 1;
-
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
-
-    if (i + 1 != high)
-    {
-        temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-        print_array(array, size);
-    }
-
-    return (i + 1);
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -51,14 +48,14 @@ size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size)
  */
 void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
 {
-    ssize_t partition_index;
+	ssize_t partition_index;
 
-    if (low < high)
-    {
-        partition_index = lomuto_partition(array, low, high, size);
-        quicksort(array, low, partition_index - 1, size);
-        quicksort(array, partition_index + 1, high, size);
-    }
+	if (low < high)
+	{
+		partition_index = lomuto_partition(array, low, high, size);
+		quicksort(array, low, partition_index - 1, size);
+		quicksort(array, partition_index + 1, high, size);
+	}
 }
 
 /**
@@ -69,8 +66,7 @@ void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-
-    quicksort(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
 }
