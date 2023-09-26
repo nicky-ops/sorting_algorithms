601d97cd4e0fd1d093248f8b1228dfd7d065c0f8
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-26 05:27:29 -0700

    fixed betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        int temp;
-        size_t j, i;
+	int temp;
+	size_t j, i;
 
-        if (array == NULL || size < 2)
-                return;
-        for (i = 0; i < size - 1; i++)
-        {
-                for (j = 0; j < size - 1; j++)
-                {
-                        if (array[j]> array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                        	print_array(array, size);
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
-        }
+	}
 }
